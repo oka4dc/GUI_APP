bdd340a4133aa498ed6b95237bfd06db0342f91e
oka4dc <58805357+oka4dc@users.noreply.github.com>
2021-05-16 16:25:08 +0100

    GUI application using python tkinkers

---
+++ my_GUI_App.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+#roots = tk.Tk()
+canvas = tk.Canvas(root, height =300, width = 500, bg="#263D42")
+canvas.pack()
+
+frame=tk.Tk.frame(root, bg="white")
+frame.place(rewidth= 0.5, relheight = 0.6, rely,relx=0.1)
+
+openfile = tk.Button
+
+root.mainloop() # run the app or display 
+
+
+print('code completed')
